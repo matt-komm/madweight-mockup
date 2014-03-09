c4e84b0304d92ec20c90313bef1a5f3da263d493
matt-komm <Matthias.Komm@cern.ch>
2014-03-09 21:30:36 +0100

    fixed error report to ctest from py file

--- tests/test-py-core.py
+++ tests/test-py-core.py
@@ -1,13 +1,20 @@
 import os
 import sys
+import unittest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'addons','swig')))
-
 import mwcore
 
-configDict = mwcore.Configuration.createEmptyDict()
-configDict.insert("test","test value 4321")
-print configDict.getString("test")
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        pass
 
-configList = mwcore.Configuration.createEmptyList()
-configList.insert("test value 1234")
-print configList.getString(0)
+    def test_Configuration_dict(self):
+        configDict = mwcore.Configuration.createEmptyDict()
+        configDict.insert("test","test value 4321")
+        self.assertEqual("test value 4321",configDict.getString("test"))
+
+    def test_Configuration_list(self):
+        configList = mwcore.Configuration.createEmptyList()
+        configList.insert("test value 1234")
+        self.assertEqual("test value 1234",configList.getString(0))
+     
